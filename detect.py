69b4d22a90579f9af7cbc18b09d0d914a32f64c7
Amit Robert Baroi <amitrobertbaroi@gmail.com>
2020-04-06 19:41:18 +0600

    Cleaned up code

--- detect.py
+++ detect.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2
 import os
+from time import time
 
 # Constants
 CONFIDENCE = 0.5
 IMG_DIR = "images"
 MODEL_DIR = "model"
 
-# Detect
+# Detect faces in images or web-cam video?
 MODE = str(input("""Detect faces in,
 - images
 - webcam
@@ -19,7 +20,7 @@ proto_txt_path = f"{MODEL_DIR}/deploy.prototxt.txt"
 model_path = "model/res10_300x300_ssd_iter_140000.caffemodel"
 net = cv2.dnn.readNetFromCaffe(prototxt=proto_txt_path, caffeModel=model_path)
 
-# TODO: add functionality to save detection video
+# TODO: add functionality to save detection video to file
 
 
 def detect_faces():
@@ -27,8 +28,12 @@ def detect_faces():
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
     # Set the blob as input of the model
     net.setInput(blob)
+
+    start = time()
     # Perform a forward pass
     detections = net.forward()
+    end = time()
+    print(f"Detection took {round(end-start, 6)} seconds")
 
     # Iterate over each detection and draw bounding boxes
     for i in range(0, detections.shape[2]):
@@ -46,39 +51,41 @@ def detect_faces():
             cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
 
 
-# Detect faces in STATIC IMAGES
-if MODE in ['image', 'images']:
-    for image_file in os.listdir(IMG_DIR):
-        image_path = os.path.join(IMG_DIR, image_file)      # Full image path
-        image = cv2.imread(image_path)                      # Load image as matrix
-        (h, w) = image.shape[:2]                            # Get height and width
-        print("[INFO] Processing: ", image_file)
-        print(f"Image shape: {h}x{w}")
+# The main program
+if __name__ == "__main__":
+    # Detect faces in STATIC IMAGES
+    if MODE in ['image', 'images']:
+        for image_file in os.listdir(IMG_DIR):
+            image_path = os.path.join(IMG_DIR, image_file)      # Full image path
+            image = cv2.imread(image_path)                      # Load image as matrix
+            (h, w) = image.shape[:2]                            # Get height and width
+            print("[INFO] Processing: ", image_file)
+            print(f"Image shape: {h}x{w}")
 
-        # Face detection
-        detect_faces()
+            # Face detection
+            detect_faces()
 
-        # Finally display the image
-        cv2.imshow(image_file, image)
-        cv2.waitKey(0)
+            # Finally display the image
+            cv2.imshow(image_file, image)
+            cv2.waitKey(0)
 
-# Detect faces in WEBCAM VIDEO
-if MODE in ['webcam', 'web-cam']:
-    cap = cv2.VideoCapture(0)
-    while True:
-        # Capture frame-by-frame
-        ret, image = cap.read()
-        (h, w) = image.shape[:2]  # Get height and width
-        print(f"Image shape: {h}x{w}")
+    # Detect faces in WEBCAM VIDEO
+    if MODE in ['webcam', 'web-cam']:
+        cap = cv2.VideoCapture(0)
+        while True:
+            # Capture frame-by-frame
+            ret, image = cap.read()
+            (h, w) = image.shape[:2]  # Get height and width
+            print(f"Image shape: {h}x{w}")
 
-        # Face detection
-        detect_faces()
+            # Face detection
+            detect_faces()
 
-        # Display the resulting frame
-        cv2.imshow("Press 'q' to quit program", image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Display the resulting frame
+            cv2.imshow("Press 'q' to quit program", image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
