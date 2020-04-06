160f22764561d3ec60055ef1544eeccda45d7b89
Amit Robert Baroi <amitrobertbaroi@gmail.com>
2020-04-06 21:57:19 +0600

    Clean up

--- detect.py
+++ detect.py
@@ -29,8 +29,8 @@ def detect_faces():
     # Set the blob as input of the model
     net.setInput(blob)
 
+    # Perform a forward pass (timed)
     start = time()
-    # Perform a forward pass
     detections = net.forward()
     end = time()
     print(f"Detection took {round(end-start, 6)} seconds")
@@ -45,7 +45,7 @@ def detect_faces():
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
             # Draw the bounding box of the face along with the associated probability
-            text = "{:.2f}%".format(confidence * 100)
+            text = f"Face : {round(confidence*100, 2)}%"
             y = startY - 10 if startY - 10 > 10 else startY + 10
             cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
             cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
